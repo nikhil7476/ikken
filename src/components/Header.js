e2129907ee6c31bb646df871f5fa2b105265428e
Nikhil Mishra <nikhil9027917476@gmail.com>
2025-02-20 23:04:07 +0530

    modified:   src/components/Header.js 	modified:   src/styles/Header.module.css 	modified:   src/styles/globals.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Nav, Container, Row, Col } from "react-bootstrap";
+import { Nav, Container, Row, Col, Navbar } from "react-bootstrap";
 import styles from "@/styles/Header.module.css";
 import { FaSearch } from "react-icons/fa";
 
@@ -10,13 +10,13 @@ const Header = () => {
     <>
       <div className={styles.hdr}>
         <Container className="pt-4 pb-4">
-          <Row>
+          <Row className={styles.hdrRow}>
             <Col
               xl={6}
               lg={6}
-              md={6}
-              sm={6}
-              xs={6}
+              md={12}
+              sm={12}
+              xs={12}
               className="align-content-center"
             >
               <Link href="/" title="Ikken" className="global-underline">
@@ -33,40 +33,48 @@ const Header = () => {
             <Col
               xl={6}
               lg={6}
-              md={6}
-              sm={6}
-              xs={6}
-              className="align-content-center"
+              md={12}
+              sm={12}
+              xs={12}
+              className={`align-content-center ${styles.hdrNav}`}
             >
-              <Nav className="justify-content-end" activeKey="/home">
-                <Nav.Item>
-                  <Nav.Link href="/" title="Home">
-                    Home
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="/about" title="About">
-                    About
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="/blog" title="Blog">
-                    Blog
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="/contact" title="Contact">
-                    Contact
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="#" title="Search">
-                    <FaSearch
-                      style={{ fontSize: "20px", marginBottom: "10px" }}
-                    />
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav>
+              <Navbar expand="lg" className="justify-content-end">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse
+                  id="basic-navbar-nav"
+                  className="justify-content-end"
+                >
+                  <Nav className={`ml-auto ${styles.hdrLnk}`}>
+                    <Nav.Item>
+                      <Nav.Link href="/" title="Home">
+                        Home
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="/about" title="About">
+                        About
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="/blog" title="Blog">
+                        Blog
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="/contact" title="Contact">
+                        Contact
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="#" title="Search">
+                        <FaSearch
+                          style={{ fontSize: "20px", marginBottom: "10px" }}
+                        />
+                      </Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Navbar.Collapse>
+              </Navbar>
             </Col>
           </Row>
         </Container>
