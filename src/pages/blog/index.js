5282ef8fdbe561603c22661ccda9c54bb3860eff
Nikhil Mishra <nikhil9027917476@gmail.com>
2025-02-25 22:54:21 +0530

    modified:   src/pages/blog/index.js 	modified:   src/styles/Blog.module.css

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,48 +1,81 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Container, Row, Col } from "react-bootstrap";
 import { blogPosts } from "@/utils/blogData"; // Assuming blogPosts are imported from a separate file
 import styles from "@/styles/Blog.module.css";
 
 const BlogPage = () => {
-  const latestPost = blogPosts[0];
-  const otherPosts = blogPosts.slice(1); // This now includes all other posts
-
   return (
-    <div className={styles.container}>
-      <h1>Writing about design and tech...</h1>
-
-      {/* Latest Post */}
-      <div className={styles.latestPost}>
-        <Image
-          src={latestPost.image}
-          alt={latestPost.title}
-          className={styles.featuredImage}
-          width={latestPost.imageWidth}
-          height={latestPost.imageHeight}
-          style={{ width: "100%", height: "auto" }}
-        />
-        <div className={styles.latestPostContent}>
-          <Link href={`/blog/${encodeURIComponent(latestPost.slug)}`}>
-            <h2 className={styles.postTitle}>{latestPost.title}</h2>
-          </Link>
-          <span className={styles.date}>{latestPost.date}</span>
-          <p className={styles.tag}>{latestPost.tag.join(" | ")}</p>
-        </div>
-      </div>
-
-      {/* All Other Posts */}
-      <div className={styles.otherPosts}>
-        {otherPosts.map((post) => (
-          <div key={post.id} className={styles.postCard}>
-            <Link href={`/blog/${encodeURIComponent(post.slug)}`}>
-              <h3 className={styles.postTitle}>{post.title}</h3>
-            </Link>
-            <span className={styles.date}>{post.date}</span>
-            <p className={styles.tag}>{post.tag.join(" | ")}</p>
-          </div>
-        ))}
-      </div>
+    <div className={styles.blgMain}>
+      <section>
+        <Container className="p-4">
+          <Row className="justify-content-center"> 
+            <Col xl={9} lg={9} md={9} sm={12} xs={12} className="text-center" >
+              <h1>
+                Ikken is a lightweight theme. Perfect for a fashion blog,
+                lifestyle blog, travel blog, food or photography blog. {"Ikken's"}
+                clean style design is the ideal choice for the minimal loving
+                creative blogger.
+              </h1>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container className="p-4">
+          {blogPosts.length > 0 ? (
+            blogPosts.map((post) => (
+              <Row
+                key={post.id}
+                className={`justify-content-between ${styles.blogPostRow}`}
+                data-aos="fade-up"
+                data-aos-duration="1500"
+              >
+                <Col xl={6} lg={6} md={6} sm={12} xs={12} className="p-0 m-0">
+                  <Link
+                    href={`/blog/${encodeURIComponent(post.slug)}`}
+                    title={post.title}
+                  >
+                    <Image
+                      src={post.image}
+                      alt={post.title}
+                      width={post.imageWidth}
+                      height={post.imageHeight}
+                      style={{
+                        width: "100%",
+                        height: "auto",
+                      }}
+                    />
+                  </Link>
+                </Col>
+                <Col
+                  xl={6}
+                  lg={6}
+                  md={6}
+                  sm={12}
+                  xs={12}
+                  className="align-content-center p-4 m-0"
+                >
+                  <span className="global-authors">By {post.author}</span>
+                  <h1 className="global-title">
+                    <Link
+                      href={`/blog/${encodeURIComponent(post.slug)}`}
+                      className="global-underline"
+                    >
+                      {post.title}
+                    </Link>
+                  </h1>
+                  <p className="global-excerpt">{post.excerpt}</p>
+                  <span className="global-tags">{post.tag.join(" | ")}</span>
+                </Col>
+              </Row>
+            ))
+          ) : (
+            <p>No blog posts available.</p>
+          )}
+        </Container>
+      </section>
     </div>
   );
 };
