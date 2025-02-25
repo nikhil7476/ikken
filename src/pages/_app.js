9e1c81b91addf9603b0550731d22c9a924d67181
Nikhil Mishra <nikhil9027917476@gmail.com>
2025-02-25 20:27:07 +0530

    modified:   src/pages/_app.js 	modified:   src/styles/globals.css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "@/styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -9,22 +9,26 @@ import { Josefin_Sans } from "next/font/google";
 
 const josefinSans = Josefin_Sans({
   subsets: ["latin"],
-  weight: ["300", "400", "600", "700"], // Adjust weights as needed
-  variable: "--font-josefin-sans", // Optional: Use CSS variable
+  weight: ["300", "400", "600", "700"],
+  className: "josefin-sans",
 });
 
 export default function App({ Component, pageProps }) {
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
+    setIsClient(true);
     AOS.init();
   }, []);
 
   return (
-    <>
-      <div className={josefinSans.variable}>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
-    </>
+    <div className={josefinSans.className}>
+      {isClient && (
+        <span className="header-description">Thoughts, stories, and ideas</span>
+      )}
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
   );
 }
