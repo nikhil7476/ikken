2fdd9e3829c1fae94811589d1fc638606454c1b6
Nikhil Mishra <nikhil9027917476@gmail.com>
2025-03-07 15:57:45 +0530

    modified:   src/components/Breadcrumbs.js

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -7,42 +7,48 @@ const Breadcrumbs = () => {
   const pathSegments = pathname.split("/").filter(Boolean);
 
   return (
-    <>
-      <nav aria-label="breadcrumb">
-        <ol
-          style={{
-            display: "flex",
-            listStyle: "none",
-            margin: 0,
-            padding: 0,
-            textTransform: "capitalize",
-          }}
-        >
-          <li>
-            <Link href="/" title="Home" className="global-underline">
-              <span>Home</span>
-            </Link>
-          </li>
-          {pathSegments.map((segment, index) => {
-            const path = `/${pathSegments.slice(0, index + 1).join("/")}`;
-            const isLast = index === pathSegments.length - 1;
+    <nav aria-label="breadcrumb">
+      <ol
+        style={{
+          display: "flex",
+          listStyle: "none",
+          margin: 0,
+          padding: 0,
+          textTransform: "capitalize",
+        }}
+      >
+        <li>
+          <Link href="/" title="Home" className="global-underline">
+            <span>Home</span>
+          </Link>
+        </li>
+        {pathSegments.map((segment, index) => {
+          const path = `/${pathSegments.slice(0, index + 1).join("/")}`;
+          const isLast = index === pathSegments.length - 1;
+          const formattedSegment = decodeURIComponent(segment).replace(
+            /-/g,
+            " "
+          );
 
-            return (
-              <li key={path}>
-                <span className="mx-2">{">"}</span>
-                {isLast ? (
-                  <span>{decodeURIComponent(segment)}</span>
-                ) : (
-                  <Link href={path} title={path} className="global-underline">
-                    <span>{decodeURIComponent(segment)}</span>
-                  </Link>
-                )}
-              </li>
-            );
-          })}
-        </ol>
-      </nav>
-    </>
+          return (
+            <li key={path}>
+              <span className="mx-2">{">"}</span>
+              {isLast ? (
+                <span>{formattedSegment}</span>
+              ) : (
+                <Link
+                  href={path}
+                  title={formattedSegment}
+                  className="global-underline"
+                >
+                  <span>{formattedSegment}</span>
+                </Link>
+              )}
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
   );
 };
 
